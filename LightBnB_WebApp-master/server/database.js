06323354ed4eb9d624e5a877ccd3846f2e798765
Dom Golac <domgolac@gmail.com>
2021-05-25 02:58:19 +0000

    update getAllProperties function to filter search

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -90,8 +90,49 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`SELECT * FROM properties LIMIT $1;`, [limit])
+  const queryParams = [];
+  
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryParams.length > 0 ? queryString += `AND ` : `WHERE `
+    queryParams.push(options.owner_id);
+    queryString += `owner_id = $${queryParams.length} `
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.length > 0 ? queryString += `AND ` : `WHERE `
+    queryParams.push(options.minimum_price_per_night * 100);
+    const minimum_price_per_night = queryParams.length;
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += `(cost_per_night >= $${minimum_price_per_night} AND cost_per_night <= $${queryParams.length}) `
+  }
+
+  if (options.minimum_rating) {
+    queryParams.length > 0 ? queryString += `AND ` : `WHERE `
+    queryParams.push(options.minimum_rating);
+    queryString += `property_reviews.rating >= $${queryParams.length} `
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  return pool.query(queryString, queryParams)
     .then((result) => {
+      console.log(result.rows);
       return result.rows;
     })
     .catch((err) => {
